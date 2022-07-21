12cec66b286799d3a1a9188c3f08e1d56a05686b
x <x>
2022-07-21 15:18:22 -0300

    proyecto 1 terminado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto 1
+
+1. Crear una pagina con todo lo visto en clase a gusto
+1. Comentar todo el html ingresado usando `<!-- comentario -->`
+1. Generar la misma pagina con javascript usando en el archivo js `document.write(`texto de html`)`
---
+++ codigo.js
@@ -0,0 +1,22 @@
+console.log("Hello waifu!");//instruccion para imprimir un mensaje en consola
+
+document.write(`<h1 style="color: green;" > Titulo  </h1>`);//insertar el "texto" dentro de los parentesis
+// si agregamos "texto" con el formato de html lo toma tal cual
+
+document.write(` 
+    <p style="color: white;"> 
+        Hola mi nombre es katsuo, este es mi primer parrafo
+        y también mi primer pagina
+    </p>
+`);
+
+document.write(` 
+    <p> 
+        Hola mi nombre es katsuo, este es mi primer parrafo
+        y también mi primer pagina
+    </p>
+`);
+
+document.write(`<img style="margin-right:500px; margin-left: 30%;" width="200px" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1617f570-4cf2-49be-b42d-924c4936628a/dckwls6-4d927c25-368e-431e-9d64-6b8eceb941ef.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2MTdmNTcwLTRjZjItNDliZS1iNDJkLTkyNGM0OTM2NjI4YVwvZGNrd2xzNi00ZDkyN2MyNS0zNjhlLTQzMWUtOWQ2NC02YjhlY2ViOTQxZWYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yv_-wgzRkT4DPn_157WG9w_2yzJC99CxzBUA2EeirAM" alt="Discord angy por que su conejita no hizo la tarea">`);
+
+document.write(`<img  style="margin-left: 30%;"   width="400px"  src="https://i.pinimg.com/originals/3a/91/ee/3a91ee6ebd3baa38d4a2b8b1be44f95b.gif" alt="discord abrazando a fluttershy">`);
